37dd85209ce6466b3c948b87c534f00fd7a2555e
Simone Baffelli <simone.baffelli@empa.ch>
2023-10-25 08:39:50 +0200

    Does not work.

--- pybis/app/test.py
+++ pybis/app/test.py
@@ -2,6 +2,11 @@ import pybis as pb
 import argparse as ap
 import requests as rq
 import base64 as b64
+import json
+import re
+import tempfile
+import pathlib as pl
+
 ap = ap.ArgumentParser()
 ap.add_argument("host", help="OpenBIS host")
 args = ap.parse_args()
@@ -11,12 +16,27 @@ o = pb.Openbis(args.host, verify_certificates=False)
 o.login("admin", "changeit")
 
 
-#Download a file
-r = rq.get("https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg", verify=False)
-r.raise_for_status()
 
-img = b64.b64encode(r.content).decode('utf-8')
-# Create a new object
-e1 = o.new_object(type="ENTRY", space="DEFAULT", project="/DEFAULT/DEFAULT", experiment="/DEFAULT/DEFAULT/DEFAULT", 
-                  props={"$document":f"""<?xml version="1.0" encoding="UTF-8"?><html><head></head><body><img src="data:image/jpg;base64, ${img}"/></body></html></xml>"""})
+
+
+
+
+# # Create a new object
+
+
+e1 = o.new_object(type="ENTRY", space="DEFAULT", project="/DEFAULT/DEFAULT", experiment="/DEFAULT/DEFAULT/DEFAULT")
 e1.save()
+
+out_file = pl.Path("./requirements.txt")
+with open(out_file, mode="w+") as tf:
+    tf.writelines("asdasdas")
+
+#Create a new dataset
+ds_new = o.new_dataset(
+    type       = 'ANALYZED_DATA',
+    experiment = '/DEFAULT/DEFAULT/DEFAULT',
+    sample     = e1.identifier,
+    files      = ["requirements.txt"],
+    props      = {'$name': 'some good name'}
+)
+ds_new.save()
